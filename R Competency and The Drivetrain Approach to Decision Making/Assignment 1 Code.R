82dbc9926ca978478f8ba928e7e8f359309de919
katjadellalibera <40602167+katjadellalibera@users.noreply.github.com>
2019-01-21 16:55:38 +0530

    adding more quantitative Analysis

--- R Competency and The Drivetrain Approach to Decision Making/Assignment 1 Code.R
+++ R Competency and The Drivetrain Approach to Decision Making/Assignment 1 Code.R
@@ -22,6 +22,8 @@ which.have.NAs <- which(is.na(mdid$CirculationDate == TRUE))
 new_mdid <- mdid[-which.have.NAs, ]%>%
   filter(CirculationDate >= "2008-01-01")
 
+## 1 ##
+# a #
 #histogram of the time differences
 new_mdid$original.duration= new_mdid$OriginalCompletionDate - new_mdid$ApprovalDate
 nas<- which(is.na(new_mdid$original.duration == TRUE))
@@ -39,5 +41,23 @@ mean(new_mdid$original.duration[-nas])
 median(new_mdid$original.duration[-nas])
 quantile(new_mdid$original.duration[-nas])
 
-#actual time diefference
+# b #
+#actual duration of the projects
 new_mdid$actual.duration=new_mdid$RevisedCompletionDate - new_mdid$ApprovalDate
+
+#plot of actual time differences
+ggplot(new_mdid, aes(x=CirculationDate, y=actual.duration))+
+  geom_point()
+
+## 2 ##
+# getting the rating percentages
+round(100*prop.table(table(new_mdid$Rating)),digits=0)
+
+## 3 ##
+# exclude PPTA projects
+non_PPTA<- new_mdid%>%
+  filter(Type!="PPTA")
+# calculate rating percentages
+round(100*prop.table(table(non_PPTA$Rating)),digits=0)
+
+## 4 ## 
